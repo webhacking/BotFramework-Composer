e2ffd0a049142e590f504e86bfa1aa325969e46c
Tony Anziano <toanzian@microsoft.com>
2020-02-20 10:50:03 -0800

    More WIP

--- Composer/packages/client/public/serviceWorker.js
+++ Composer/packages/client/public/serviceWorker.js
@@ -1,9 +1,19 @@
-self.addEventListener('install', async ev => {
-  await ev.waitUntil(
-    new Promise(resolve => {
+self.addEventListener('install', ev => {
+  ev.waitUntil(async function() {
+    await new Promise(resolve => {
       console.log('installing...');
       resolve();
-    })
-  );
-  console.log('installed...');
+    });
+    console.log('installed...');
+  });
+});
+
+self.addEventListener('fetch', ev => {
+  ev.waitUntil(async function() {
+    await new Promise(resolve => {
+      console.log(ev);
+      console.log(ev.request);
+      resolve();
+    });
+  });
 });
