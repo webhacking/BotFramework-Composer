4ba9528cb49157f6e35f3805c3933ad3e6892421
Tony Anziano <tonyanziano5@gmail.com>
2020-02-20 21:17:42 -0800

    Added button to update service worker.

--- Composer/packages/client/src/serviceWorker/init.ts
+++ Composer/packages/client/src/serviceWorker/init.ts
@@ -5,8 +5,27 @@ export async function initServiceWorker() {
     try {
       const registration = await serviceWorker.register('/serviceWorker.js');
       console.log('Registration successful: ', registration);
+      createButtonAndHookUpEvent(registration);
     } catch (e) {
       console.error('Registration failed: ', e);
     }
+  }
+}
+
+function createButtonAndHookUpEvent(registration) {
+  const button = document.querySelector('#update-sw-button');
+  if (!button) {
+    const newButton = document.createElement('button');
+    newButton.value = 'Restart SW';
+    newButton.addEventListener('click', async ev => {
+      await registration.update();
+      console.log('updated sw');
+    });
+    newButton.setAttribute(
+      'style',
+      `position: absolute; bottom: 0; left: 0; height: 50px; width: 50px; background-color: gray;`
+    );
+
+    document.body.appendChild(newButton);
   }
 }
