2a5ec3764a2f1db261a4497c1f43689ce3f50125
Srinaath Ravichandran <srravich@microsoft.com>
2020-04-15 01:49:50 -0700

    Safe commit for electron
    Signed-off-by: Srinaath Ravichandran <srravich@microsoft.com>

--- Composer/packages/electron-server/src/utility/logger.ts
+++ Composer/packages/electron-server/src/utility/logger.ts
@@ -5,15 +5,16 @@ import fs from 'fs';
 
 import debug from 'debug';
 
-// import ElectronWindow from '../electronWindow';
+import ElectronWindow from '../electronWindow';
 
 export default debug('composer');
 
 export function log(arg1, arg2 = '') {
-  // if (ElectronWindow.isBrowserWindowCreated) {
-  //   ElectronWindow.getInstance().browserWindow.webContents.executeJavaScript(`console.log("${args}")`);
-  // }
-  fs.appendFile('/Users/srravich/Desktop/helloworld.txt', `\n${arg1} --- ${arg2}`, function(err) {
+  if (ElectronWindow.isBrowserWindowCreated) {
+    ElectronWindow.getInstance().browserWindow.webContents.executeJavaScript(`console.log("${arg1} --- ${arg2}")`);
+  }
+  console.log(`${arg1} --- ${arg2}`);
+  fs.appendFile('helloworld.txt', `\n${arg1} --- ${arg2}`, function(err) {
     if (err) return console.log(err);
   });
 }
